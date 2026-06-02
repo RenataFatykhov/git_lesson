6abf3063a0920a3c0f8fdeb7e2238fb5987ff500
RenataFatykhov <renata.musenova@gmail.com>
2026-06-02 11:56:50 +0300

    changed greeting

--- src/main/java/com/mycompany/myapp/Main.java
+++ src/main/java/com/mycompany/myapp/Main.java
@@ -2,6 +2,7 @@ package com.mycompany.myapp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, qa guru!");
+        System.out.print("Hello");
+        System.out.println(", qa guru!");
     }
 }
